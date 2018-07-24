e33cbd5b43b29967b09a149c824d5771a158e9f2
He Qian Wang <heqianw@gmail.com>
2018-07-23 22:10:03 -0400

    pointers exercise

--- practice/pointers.c
+++ practice/pointers.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// void main(int argc, char* argv[]){
+//     char string1[] = "hello";
+//     char string2[] = "World";
+//     //char *string3 = &string2;
+//     int i;
+//     for(i = 0; i < 5; i++){
+//         char *first = &string1[i];
+//         *first = string2[i];   
+//     }
+//     printf("%s\n",string1);
+// }
+
 void main(int argc, char* argv[]){
-    char string1[] = "hello";
-    char string2[] = "World";
-    //char *string3 = &string2;
-    int i;
-    for(i = 0; i < 5; i++){
-        char *first = &string1[i];
-        *first = string2[i];   
+    char string[] = "COMP 206 is almost my favorite course.";
+    int i = 0, j =0;
+    int spaceCount = 0;
+    int counter = 0;
+    //char *newString;
+    while(string[i] != '\0'){
+        if(string[i] == ' '){
+            spaceCount++;
+        }
+        if(spaceCount == 3){
+            counter++;
+        }
+        else{
+            string[j] = string[i];
+            j++;
+        }
+        i++;
     }
-    printf("%s\n",string1);
+    string[i - counter] = '\0';
+    printf("%d\n", j);
+    printf("%s\n", string);
 }
